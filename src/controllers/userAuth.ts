2e871f8977afa1ae14b7b7f94f5a58d84469fe0f
Patrick <ptemy5@gmail.com>
2024-01-15 04:01:36 -0300

    user auth

---
+++ src/controllers/userAuth.ts
@@ -0,0 +1,33 @@
+import knex from '../config/connect'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { User } from '../models/userType'
+import { Request, Response } from 'express'
+
+export const userLogin = async (req: Request, res: Response) => {
+  const { email, password } = req.body
+
+  try {
+    const user = await knex<User>('users').where({ email }).first()
+
+    if (!user) {
+      return res.status(400).json({ message: 'Usuário não encontrado' })
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password)
+
+    if (!isValidPassword) {
+      return res.status(400).json({ message: 'Senha inválida' })
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.SECRET as string, {
+      expiresIn: '1d'
+    })
+
+    const { password: _, ...userInfo } = user
+
+    return res.status(200).json({ user: userInfo, token })
+  } catch {
+    return res.status(400).json({ message: 'Erro ao realizar login' })
+  }
+}
